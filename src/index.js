3d02b1978823d0943d7f109184b3ed3aec51cb8a
Ritesh Sangwan <sangwan.ritesh@yahoo.in>
2024-01-21 17:15:17 +0530

    sdc-4

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@
 
 // import the bmi module here
 const bmiModule = require('./bmi');
+const isPrime = require('quick-is-prime');
 
 const height = 180;
 const weight = 80;
@@ -15,4 +16,13 @@ function bmi() {
     console.log('Your BMI is ' + bmi);
 }
 
+function primeNumbers() {
+    console.log(isPrime(7));
+    console.log(isPrime(77777));
+    console.log(isPrime(89876767))
+    console.log(isPrime(91));
+}
+
 bmi();
+
+primeNumbers();
